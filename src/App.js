91e6e8f1849c9b77bf66cf9fb2026df3997c2ee5
Rasmus Bouvin <Rasmusbouvin@hotmail.com>
2023-03-21 19:48:28 +0100

    removed unused components that were imported

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import "./App.css";
-import React, { useState } from 'react';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { useState } from "react";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import NavBar from "./Components/Navbar";
-import PieChart from "./Components/PieChart";
-import ScatterPlot from "./Components/ScatterCharts";
-import BarChart from "./Components/BarChart";
 import DataTablePageOne from "./Components/DataTable";
-import DataTablePageTwo from "./Components/DataTable2";
-import DataTablePageThree from "./Components/DataTable3";
-import LineChart from "./Components/LineChart";
-
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -21,12 +14,12 @@ function App() {
 
   const theme = createTheme({
     palette: {
-      type: darkMode ? 'dark' : 'light',
+      type: darkMode ? "dark" : "light",
     },
   });
 
   const styles = {
-    textColor: darkMode ? '#fff' : '#000',
+    textColor: darkMode ? "#fff" : "#000",
   };
 
   return (
@@ -34,8 +27,6 @@ function App() {
       <CssBaseline />
       <NavBar darkMode={darkMode} handleDarkMode={handleDarkMode} />
       <DataTablePageOne textColor={styles.textColor} />
-   
-      
     </ThemeProvider>
   );
 }
